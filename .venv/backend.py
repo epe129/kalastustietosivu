6db5e074eab5e1b26c5d1de1cb2a6a832777ebae
epe129 <eemelik985@gmail.com>
2026-01-23 11:27:20 +0200

    commit

--- .venv/backend.py
+++ .venv/backend.py
@@ -17,36 +17,57 @@ def get_input():
     pituus = pituus_input.get()
     paino = paino_input.get()
     laji = laji_input.get()
+    if laji == "muu":
+        laji = laji_input_muu.get()
     saatu_aika = aika.get_date()
     paikka = paikka_input.get()
     viehe = viehe_input.get()
     vapa = vapa_input.get()
-    print(laji)
-    # cursor.execute(f'INSERT INTO kalastaja (nimi) VALUES ("{nimi}")')        
+    # print(nimi, pituus, paino, laji, saatu_aika, paikka, viehe, vapa)
+    cursor.execute(f'INSERT INTO kalastaja (nimi) VALUES ("{nimi}")')        
     # saa aina edellisen taulun id:n
-    # kalastaja_id = cursor.lastrowid
-    # cursor.execute(f'INSERT INTO viehe (viehe) VALUES ("{viehe}")')
-    # viehe_id = cursor.lastrowid
-    # cursor.execute(f'INSERT INTO vapa (vapa) VALUES ("{vapa}")')
-    # vapa_id = cursor.lastrowid
-    # cursor.execute(f'INSERT INTO laji (laji) VALUES ("{laji}")')
-    # laji_id = cursor.lastrowid
-    # cursor.execute(f'INSERT INTO tarppi (aika, kalastaja_id, viehe_id, vapa_id, paikka) VALUES ("{saatu_aika}", "{kalastaja_id}", "{viehe_id}", "{vapa_id}", "{paikka}")')
-    # tarppi_id = cursor.lastrowid
-    # cursor.execute(f'INSERT INTO kala (tarppi_id, pituus, paino, laji_id) VALUES ("{tarppi_id}", "{pituus}", "{paino}", "{laji_id}")')
-    # # tallettaa tapahtuneen tietokantaan
-    # connection.commit()
-    # # sulkee yhteyden tietokannan tauluihin
-    # cursor.close()
+    kalastaja_id = cursor.lastrowid
+    cursor.execute(f'INSERT INTO viehe (viehe) VALUES ("{viehe}")')
+    viehe_id = cursor.lastrowid
+    cursor.execute(f'INSERT INTO vapa (vapa) VALUES ("{vapa}")')
+    vapa_id = cursor.lastrowid
+    cursor.execute(f'INSERT INTO laji (laji) VALUES ("{laji}")')
+    laji_id = cursor.lastrowid
+    cursor.execute(f'INSERT INTO tarppi (aika, kalastaja_id, viehe_id, vapa_id, paikka) VALUES ("{saatu_aika}", "{kalastaja_id}", "{viehe_id}", "{vapa_id}", "{paikka}")')
+    tarppi_id = cursor.lastrowid
+    cursor.execute(f'INSERT INTO kala (tarppi_id, pituus, paino, laji_id) VALUES ("{tarppi_id}", "{pituus}", "{paino}", "{laji_id}")')
+    # tallettaa tapahtuneen tietokantaan
+    connection.commit()
+    # sulkee yhteyden tietokannan tauluihin
+    cursor.close()
+
+# saa näytön leveyden
+window_width = root.winfo_width()
+
+# lasketaan x(leveys suunta) coordinaatiot keskelle sivua
+x = (window_width + 400)
+z = (window_width + 400)
+nimi_paikka = (window_width + 350)
+pituus_paikka = (window_width + 295)
+paino_paikka = (window_width + 305)
+laji_paikka = (window_width + 265)
+aika_paikka = (window_width + 350)
+paikka_paikka = (window_width + 330)
+viehe_paikka = (window_width + 340)
+vapa_paikka = (window_width + 340)
+button_paikka = (window_width + 505)
+laji_muu_paikka = (window_width + 315)
+
 # otsikko
 l = tk.Label(root, text = "Kalastustiedot", font=('calibre',20,'bold'))
-l.pack()
+l.place(x=x, y=25)
 
 # luodaan inpu teille tyyppi
 nimi_var=tk.StringVar()
 pituus_var=tk.IntVar()
 paino_var=tk.IntVar()
 laji_var=tk.StringVar()
+laji_var_muu=tk.StringVar()
 aika_var=tk.StringVar()
 paikka_var=tk.StringVar()
 viehe_var=tk.StringVar()
@@ -55,64 +76,70 @@ vapa_var=tk.StringVar()
 # luodaan inputit ja labelit
 nimi = tk.Label(root, text="Nimi:", font=('calibre',15))
 nimi_input = tk.Entry(root, textvariable=nimi_var, font=('calibre',15,'normal'))
-nimi.pack()
-nimi_input.pack()
+nimi.place(x=nimi_paikka, y=70)
+nimi_input.place(x=x, y=70)
 
 pituus = tk.Label(root, text="Pituus(cm):", font=('calibre',15))
 pituus_input = tk.Entry(root, textvariable=pituus_var, font=('calibre',15,'normal'))
-pituus.pack()
-pituus_input.pack()
+pituus.place(x=pituus_paikka, y=110)
+pituus_input.place(x=x, y=110)
 
 paino = tk.Label(root, text="Paino(kg):", font=('calibre',15))
 paino_input = tk.Entry(root, textvariable=paino_var, font=('calibre',15,'normal'))
-paino.pack()
-paino_input.pack()
+paino.place(x=paino_paikka, y=150)
+paino_input.place(x=x, y=150)
 
 laji = tk.Label(root, text="Valiset kalalaji:", font=('calibre',15))
-laji.pack()
+laji.place(x=laji_paikka, y=190)
 
 luettelo_lajit = ["ahven", "hauki", "kuha", "siika", "taimen", "made", "lohi", "muu"]
 
 laji_input = ttk.Combobox(root, values=luettelo_lajit, font=('calibre',15), textvariable=laji_var)
 laji_input.set("Valiset kalalaji")
-laji_input.pack()
+laji_input.place(x=x, y=190)
 
 def get_index(*args):
     # saa arvon joka valittu
     if str(laji_var.get()) == "muu":
-        laji_input = tk.Entry(root, textvariable=laji_var, font=('calibre',15,'normal'))
-        laji_input.pack()
+        global laji_input_muu 
+        laji_muu = tk.Label(root, text="Mikä laji:", font=('calibre',15))
+        laji_input_muu = tk.Entry(root, textvariable=laji_var_muu, font=('calibre',15,'normal'))
+        laji_input_muu.place(x=x, y=230)
+        laji_muu.place(x=laji_muu_paikka, y=230)
 
 # katsoo mikä arvo on valittu
 laji_var.trace('w', get_index)
 print(str(laji_var.get()))
 
 aika_text = tk.Label(root, text="Aika:", font=('calibre',15))
-aika = DateEntry(root, width=12, background="darkblue", foreground="white", borderwidth=2, font=('calibre',15,'normal'))
+aika = DateEntry(root, width=12, background="darkblue", foreground="white", borderwidth=2, font=('calibre',15,'normal'), date_pattern="dd.mm.yyyy")
 aika_input = tk.Entry(root, textvariable=aika_var, font=('calibre',15,'normal'))
-aika_text.pack()
-aika.pack()
+aika_text.place(x=aika_paikka, y=270)
+aika.place(x=x, y=270)
 
 paikka = tk.Label(root, text="Paikka:", font=('calibre',15))
 paikka_input = tk.Entry(root, textvariable=paikka_var, font=('calibre',15,'normal'))
-paikka.pack()
-paikka_input.pack()
+paikka.place(x=paikka_paikka, y=310)
+paikka_input.place(x=x, y=310)
 
 viehe = tk.Label(root, text="Viehe:", font=('calibre',15))
 viehe_input = tk.Entry(root, textvariable=viehe_var, font=('calibre',15,'normal'))
-viehe.pack()
-viehe_input.pack()
+viehe.place(x=viehe_paikka, y=350)
+viehe_input.place(x=x, y=350)
 
 vapa = tk.Label(root, text="Vapa:", font=('calibre',15))
 vapa_input = tk.Entry(root, textvariable=vapa_var, font=('calibre',15,'normal'))
-vapa.pack()
-vapa_input.pack()
+vapa.place(x=vapa_paikka, y=390)
+vapa_input.place(x=x, y=390)
+# tyylit napille
+style = ttk.Style()
 
-border = LabelFrame(root, borderwidth=2, background="black")
-border.pack(pady = 10)
-            
-button = tk.Button(border, text="Lähetä", command=get_input, font=('calibre',15,'normal'), cursor="hand2")
-button.pack()
+style.configure('TButton', font = 
+               ('calibri', 15, 'bold'),
+                    borderwidth = '4')
+
+button = ttk.Button(text="Lähetä", command=get_input, style='TButton', cursor="hand2")
+button.place(x=button_paikka, y=425)
 
 if __name__=="__main__":
     root.mainloop()
