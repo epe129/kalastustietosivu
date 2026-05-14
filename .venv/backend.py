cccf10b20ef278b95c339915f1424d9abd93b50c
epe129 <eemelik985@gmail.com>
2026-05-14 14:28:23 +0300

    fixes to backend.py

--- .venv/backend.py
+++ .venv/backend.py
@@ -1,3 +1,5 @@
+# muista asentaa: 
+# pip install CTkListbox
 # importataan kaikki mitä tarvii
 import bcrypt
 import customtkinter as ctk
@@ -5,16 +7,10 @@ import pymysql, dbinfo
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import *
+from CTkListbox import CTkListbox
 
-# otetaan db tiedot python tiedostosta
-USER = dbinfo.data["USER"]
-PASSWORD = dbinfo.data["PASSWORD"]
-DBNIMI = dbinfo.data["DBNIMI"]
-PORT = dbinfo.data["PORT"]
-HOST = dbinfo.data["HOST"]
-
-# yhteys tietokantaan
-connection = pymysql.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, database=DBNIMI)
+# yhteys tietokantaan, ottaa yhteyden tiedot python tidostosta
+connection = pymysql.connect(host=dbinfo.data["HOST"], port=dbinfo.data["PORT"], user=dbinfo.data["USER"], password=dbinfo.data["PASSWORD"], database=dbinfo.data["DBNIMI"])
 cursor = connection.cursor()
 
 # laitta ohjelmalle systeemin 
@@ -22,8 +18,11 @@ ctk.set_appearance_mode("System")
 
 # luodaan ikkuna
 root = ctk.CTk()
+root.geometry("1000x600")
 root.resizable(width=False, height=False)
-root.geometry("600x600")
+# root.minsize(1000, 600)
+# root.maxsize(1000, 600)
+# root.update_idletasks()
 root.title("Admin")
 
 def admin_window():
@@ -33,15 +32,15 @@ def admin_window():
     vavat_list = []
     viehet_list = []
 
-    # laitta ohjelmalle systeemin, laitetaan uudestaan koska on uusi ikkuna
-    ctk.set_appearance_mode("System") 
-
     # luodaan ikkuna
-    admin_window = ctk.CTk()
+    admin_window = ctk.CTkToplevel(root)
     admin_window.geometry("1000x600")  
     admin_window.resizable(width=False, height=False)
     admin_window.title("Admin")
-
+    # luodaan frame, kaikki admin_window buttonit, label tms ob täb sisällä jotta pysyvät aina samassa paikassa
+    container = ctk.CTkFrame(admin_window, width=1000, height=600)
+    container.place(x=0, y=0)
+    
     # tapahtuu käyttäjän poisto
     def kayttaja_poista():
         # tarkistaa kummasta ottaa arvon input vai valikosta 
@@ -128,7 +127,7 @@ def admin_window():
         vapa_input.configure(values=[x[0] for x in vavat])
         vapa_input.set("Poista vapa")
         vavat_list.clear()
-        for x in vavat_list:
+        for x in vavat:
             vavat_list.append(x[0])
 
     # tapahtuu viehen poisto
@@ -181,29 +180,33 @@ def admin_window():
 
     # päivitää listaa joka näkyy kun hakee inputissa
     def paivittaa_list_kayttaja(kayttajat_list):
-        # poistaa kaiken inputista
-        kayttajat_list_box.delete(0, END)
+        # poistaa kaiken inputista, näin jotta käy yksitellen jokaisen läpi niin ei tuu erroria
+        for _ in range(kayttajat_list_box.size()):
+            kayttajat_list_box.delete(0)
         # lisää arvot uudestaan listaa jotta päivittyy
         for item in kayttajat_list:
             kayttajat_list_box.insert(END, item)
     # päivitää listaa joka näkyy kun hakee inputissa
     def paivittaa_list_laji(lajit_list):
-        # poistaa kaiken inputista
-        laji_list_box.delete(0, END)
+        # poistaa kaiken inputista        
+        for _ in range(laji_list_box.size()):
+            laji_list_box.delete(0)        
         # lisää arvot uudestaan listaa jotta päivittyy
         for item in lajit_list:
             laji_list_box.insert(END, item)
     # päivitää listaa joka näkyy kun hakee inputissa
     def paivittaa_list_viehe(viehet_list):
         # poistaa kaiken inputista
-        viehe_list_box.delete(0, END)
+        for _ in range(viehe_list_box.size()):
+            viehe_list_box.delete(0)
         # lisää arvot uudestaan listaa jotta päivittyy
         for item in viehet_list:
             viehe_list_box.insert(END, item)
     # päivitää listaa joka näkyy kun hakee inputissa
     def paivittaa_list_vapa(vapa_list):
         # poistaa kaiken inputista
-        vapa_list_box.delete(0, END)
+        for _ in range(vapa_list_box.size()):
+            vapa_list_box.delete(0)
         # lisää arvot uudestaan listaa jotta päivittyy
         for item in vapa_list:
             vapa_list_box.insert(END, item)
@@ -215,25 +218,25 @@ def admin_window():
         # poistaa kaiken inputista
         hae_kayttaja.delete(0, END)
         # lisää klikatun arvon inputtiin
-        hae_kayttaja.insert(0, kayttajat_list_box.get(ANCHOR))
+        hae_kayttaja.insert(0, kayttajat_list_box.get())
     
     def tayttaa_input_laji(e):
         # poistaa kaiken inputista
         hae_laji.delete(0, END)
         # lisää klikatun arvon inputtiin
-        hae_laji.insert(0, laji_list_box.get(ANCHOR))
+        hae_laji.insert(0, laji_list_box.get())
 
     def tayttaa_input_viehe(e):
         # poistaa kaiken inputista
         hae_viehe.delete(0, END)
         # lisää klikatun arvon inputtiin
-        hae_viehe.insert(0, viehe_list_box.get(ANCHOR))
+        hae_viehe.insert(0, viehe_list_box.get())
 
     def tayttaa_input_vapa(e):
         # poistaa kaiken inputista
         hae_vapa.delete(0, END)
         # lisää klikatun arvon inputtiin
-        hae_vapa.insert(0, vapa_list_box.get(ANCHOR))
+        hae_vapa.insert(0, vapa_list_box.get())
 
     # ------------------------------------------------------------------ #
 
@@ -243,7 +246,8 @@ def admin_window():
         hae_kayttaja_input = hae_kayttaja.get()
         kayttajat_list_box.place(x=210, y=165)
         kayttajat_input.place(x=-210, y=-165)
-        button_kayttaja.place(x=443, y=128)
+        button_kayttaja.place(x=423, y=130)
+
         # laitetaan vapa teksti pois tieltä
         text_vapa.place(x=-210, y=-190)
         hae_vapa.place(x=-210, y=-190) 
@@ -278,7 +282,7 @@ def admin_window():
         laji_kayttaja_input = hae_laji.get()
         laji_list_box.place(x=590, y=165)
         laji_input.place(x=-210, y=-165)
-        button_laji.place(x=823, y=128)
+        button_laji.place(x=803, y=130)
         # laitetaan viehe teksti pois tieltä
         text_viehe.place(x=-210, y=-190)
         hae_viehe.place(x=-210, y=-190) 
@@ -304,7 +308,7 @@ def admin_window():
             data_laji = []
             for item in lajit_list:
                 if laji_kayttaja_input.lower() in str(item).lower():
-                    data_laji.append(item)        
+                    data_laji.append(item)     
         # päivittää listaa joka näkyy kun hakee haun perusteella
         paivittaa_list_laji(data_laji)
     
@@ -313,7 +317,7 @@ def admin_window():
         viehe_kayttaja_input = hae_viehe.get()
         viehe_list_box.place(x=590, y=310)
         viehe_input.place(x=-210, y=-165)
-        button_viehe.place(x=823, y=280)
+        button_viehe.place(x=803, y=280)
         # jos haku kenttä on tyhjä laittaa buttonit ja muut inputit/tekstit takaisin paikoilleen 
         if viehe_kayttaja_input == '':
             data_viehe = viehet_list
@@ -336,7 +340,7 @@ def admin_window():
         vapa_input_hae = hae_vapa.get()
         vapa_list_box.place(x=210, y=310)
         vapa_input.place(x=-210, y=-165)
-        button_vapa.place(x=443, y=280)
+        button_vapa.place(x=423, y=280)
         # jos haku kenttä on tyhjä laittaa buttonit ja muut inputit/tekstit takaisin paikoilleen 
         if vapa_input_hae == '':
             data_vapa = vavat_list
@@ -355,64 +359,70 @@ def admin_window():
         paivittaa_list_vapa(data_vapa)
 
     # ------------------------------------------------------------------ #
+    # luodaan inpu teille tyyppi
+    hae_string_var = StringVar()
+    laji_hae_string_var = StringVar()
+    vapa_hae_string_var = StringVar()
+    viehe_hae_string_var = StringVar()
+
+    # ------------------------------------------------------------------ #
 
     # koko ikkunan otsikko
-    text_admin = ctk.CTkLabel(admin_window, text="Admin", font=('calibre',40))
+    text_admin = ctk.CTkLabel(container, text="Admin", font=('calibre',40))
     text_admin.place(x=450, y=25)
 
     # ------------------------------------------------------------------ #
 
     # alkaa käyttäjä 
     # otsikko
-    text_kayttaja = ctk.CTkLabel(admin_window, text="Poista käyttäjä:", font=('calibre',20))
+    text_kayttaja = ctk.CTkLabel(container, text="Poista käyttäjä:", font=('calibre',20))
     text_kayttaja.place(x=210, y=100)
 
     # luodaan luettelo jossa näkyy arvot jos käyttää haku kenttää
-    kayttajat_list_box = Listbox(admin_window, width=50)
+    kayttajat_list_box = CTkListbox(container, width=200)
 
     # hakukenttä
-    hae_string_var = StringVar()
-    hae_kayttaja = tk.Entry(admin_window, textvariable=hae_string_var, font=('calibre',12,'normal'), width=25)
+    hae_kayttaja = ctk.CTkEntry(container, textvariable=hae_string_var, font=('calibre',12,'normal'), width=200)
     hae_kayttaja.place(x=210, y=130)        
 
     # luettelo boxsi
-    kayttajat_input = ctk.CTkComboBox(admin_window, values=[x[0] for x in kayttajat], font=('calibre',15))
+    kayttajat_input = ctk.CTkComboBox(container, values=[x[0] for x in kayttajat], font=('calibre',15))
     kayttajat_input.set("Poista käyttäjä")
     kayttajat_input.place(x=210, y=160)
-    
+
     # button
-    button_kayttaja = ctk.CTkButton(master=admin_window ,text="Poista käyttäjä", command=kayttaja_poista)
+    button_kayttaja = ctk.CTkButton(master=container ,text="Poista käyttäjä", command=kayttaja_poista)
     button_kayttaja.place(x=210, y=190)
+    
 
     # kuuntelee jos kayttajat_list_box arvo klikattu
     kayttajat_list_box.bind("<Button-1>", tayttaa_input_kayttaja)
     # päivittää kayttajat_list_box
     paivittaa_list_kayttaja(kayttajat_list)    
     # kuuntelee jos inputtiin kirjoitetaan
-    hae_kayttaja.bind('<Key>', tarkistaa_input_kayttaja)
+    hae_kayttaja.bind('<KeyRelease>', tarkistaa_input_kayttaja)
     
     # ------------------------------------------------------------------ #
 
     # alkaa laji
     # otsikko
-    text_laji = ctk.CTkLabel(admin_window, text="Poista laji:", font=('calibre',20))
+    text_laji = ctk.CTkLabel(container, text="Poista laji:", font=('calibre',20))
     text_laji.place(x=590, y=100)
     
     # luodaan luettelo jossa näkyy arvot jos käyttää haku kenttää
-    laji_list_box = Listbox(admin_window, width=50)
+    laji_list_box = CTkListbox(container, width=200)
 
     # hakukenttä
-    laji_hae_string_var = StringVar()
-    hae_laji = tk.Entry(admin_window, textvariable=laji_hae_string_var, font=('calibre',12,'normal'), width=25)
+    hae_laji = ctk.CTkEntry(container, textvariable=laji_hae_string_var, font=('calibre',12,'normal'), width=200)
     hae_laji.place(x=590, y=130) 
 
     # luettelo boxsi
-    laji_input = ctk.CTkComboBox(admin_window, values=[x[0] for x in lajit], font=('calibre',15))
+    laji_input = ctk.CTkComboBox(container, values=[x[0] for x in lajit], font=('calibre',15))
     laji_input.set("Poista laji")
     laji_input.place(x=590, y=160)
 
     # button
-    button_laji = ctk.CTkButton(master=admin_window ,text="Lähetä", command=laji_poista)
+    button_laji = ctk.CTkButton(master=container ,text="Poista laji", command=laji_poista)
     button_laji.place(x=590, y=190)
 
     # kuuntelee jos laji_list_box arvo klikattu
@@ -420,30 +430,29 @@ def admin_window():
     # päivittää laji_list_box
     paivittaa_list_laji(lajit_list)    
     # kuuntelee jos inputtiin kirjoitetaan
-    hae_laji.bind('<Key>', tarkistaa_input_laji)
+    hae_laji.bind('<KeyRelease>', tarkistaa_input_laji)
 
     # ------------------------------------------------------------------ #
 
     # alkaa vapa 
     # otsikko
-    text_vapa = ctk.CTkLabel(admin_window, text="Poista vapa:", font=('calibre',20))
+    text_vapa = ctk.CTkLabel(container, text="Poista vapa:", font=('calibre',20))
     text_vapa.place(x=210, y=250)
 
     # luodaan luettelo jossa näkyy arvot jos käyttää haku kenttää
-    vapa_list_box = Listbox(admin_window, width=50)
-    
+    vapa_list_box = CTkListbox(container, width=200)
+
     # hakukenttä
-    vapa_hae_string_var = StringVar()
-    hae_vapa = tk.Entry(admin_window, textvariable=vapa_hae_string_var, font=('calibre',12,'normal'), width=25)
+    hae_vapa = ctk.CTkEntry(container, textvariable=vapa_hae_string_var, font=('calibre',12,'normal'), width=200)
     hae_vapa.place(x=210, y=280) 
 
     # luettelo boxsi
-    vapa_input = ctk.CTkComboBox(admin_window, values=[x[0] for x in vavat], font=('calibre',15))
+    vapa_input = ctk.CTkComboBox(container, values=[x[0] for x in vavat], font=('calibre',15))
     vapa_input.set("Poista vapa")
     vapa_input.place(x=210, y=310)
     
     # button
-    button_vapa = ctk.CTkButton(master=admin_window ,text="Lähetä", command=vapa_poista)
+    button_vapa = ctk.CTkButton(master=container ,text="Poista vapa", command=vapa_poista)
     button_vapa.place(x=210, y=340)
 
     # kuuntelee jos vapa_list_box arvo klikattu
@@ -451,30 +460,29 @@ def admin_window():
     # päivittää vapa_list_box
     paivittaa_list_vapa(vavat_list)    
     # kuuntelee jos inputtiin kirjoitetaan
-    hae_vapa.bind('<Key>', tarkistaa_input_vapa)
+    hae_vapa.bind('<KeyRelease>', tarkistaa_input_vapa)
 
     # ------------------------------------------------------------------ #
 
     # alkaa viehe
     # otsikko
-    text_viehe = ctk.CTkLabel(admin_window, text="Poista viehe:", font=('calibre',20))
+    text_viehe = ctk.CTkLabel(container, text="Poista viehe:", font=('calibre',20))
     text_viehe.place(x=590, y=250)
 
     # luodaan luettelo jossa näkyy arvot jos käyttää haku kenttää
-    viehe_list_box = Listbox(admin_window, width=50)
+    viehe_list_box = CTkListbox(container, width=200)
     
     # hakukenttä
-    viehe_hae_string_var = StringVar()
-    hae_viehe = tk.Entry(admin_window, textvariable=viehe_hae_string_var, font=('calibre',12,'normal'), width=25)
+    hae_viehe = ctk.CTkEntry(container, textvariable=viehe_hae_string_var, font=('calibre',12,'normal'), width=200)
     hae_viehe.place(x=590, y=280) 
 
     # luettelo boxsi
-    viehe_input = ctk.CTkComboBox(admin_window, values=[x[0] for x in viehet], font=('calibre',15))
+    viehe_input = ctk.CTkComboBox(container, values=[x[0] for x in viehet], font=('calibre',15))
     viehe_input.set("Poista viehe")
     viehe_input.place(x=590, y=310)
     
     # button
-    button_viehe = ctk.CTkButton(master=admin_window ,text="Lähetä", command=viehe_poista)
+    button_viehe = ctk.CTkButton(master=container ,text="Poista viehe", command=viehe_poista)
     button_viehe.place(x=590, y=340)
     
     # kuuntelee jos viehe_list_box arvo klikattu
@@ -482,70 +490,65 @@ def admin_window():
     # päivittää viehe_list_box
     paivittaa_list_viehe(viehet_list)    
     # kuuntelee jos inputtiin kirjoitetaan
-    hae_viehe.bind('<Key>', tarkistaa_input_viehe)
+    hae_viehe.bind('<KeyRelease>', tarkistaa_input_viehe)
 
     # ------------------------------------------------------------------ #
 
     # jos painaa x:sää sulkee ikkunan, pitää olla molemmat koska root.withdraw() ei sulje ikkunaa kokonaan vain piilottaa
     def close():
-        admin_window.destroy()
         root.destroy()
     admin_window.protocol("WM_DELETE_WINDOW", close)
-    admin_window.mainloop()
 
 def get_input():
     try:
         # saadaan inputit
         username = username_input.get()
-        password = password_input.get()        
+        password = password_input.get()     
         # tarkistaa onko salasana ja käyttäjänimi oikein
         # ei saa oikeasti tehäd näin jos olisi tuotannossa
         if username == dbinfo.data["admin_username"] and bcrypt.checkpw(password.encode("utf-8"), dbinfo.data["admin_password"]):
             # sulkee log ikkunan
             root.withdraw()
-            # avaa "hallinta ikkunan"
+            # avaa admin_window
             admin_window()
         else:
             # jos salasana tai käyttäjänimi väärin laittaa tekstin
-            text.place(x=window_width + 20, y=170)
+            text.place(x=385, y=235)
             my_string_var.set("Salasana tai käyttäjänimi on väärin")
     except Exception as e:
         # jos jokin menee pieleen tulee teksti
-        text.place(x=window_width + 70, y=170)
+        text.place(x=465, y=235)
         my_string_var.set("Jokin meni vikaan")
     
-# saa näytön leveyden
-window_width = root.winfo_width()
-
 # luodaan inpu teille tyyppi
-username_var=tk.StringVar()
-password_var=tk.StringVar()
+username_var= StringVar()
+password_var= StringVar()
 my_string_var = StringVar()
 
 # otsikko teksti
-l = ctk.CTkLabel(root, text = "Log in", font=('calibre',25,'bold'))
-l.place(x=window_width + 100, y=75)
+l = ctk.CTkLabel(root, text = "Log in", font=('calibre',35,'bold'))
+l.place(x=465, y=75)
 
 # name input
-username = ctk.CTkLabel(root, text="Name:", font=('calibre',15))
-username_input = tk.Entry(root, textvariable=username_var, font=('calibre',12,'normal'), width=25)
-username.place(x=window_width - 23, y=115)
-username_input.place(x=window_width + 25, y=115)
+username = ctk.CTkLabel(root, text="Name:", font=('calibre',20))
+username_input = ctk.CTkEntry(root, textvariable=username_var, font=('calibre',20,'normal'), width=200)
+username.place(x=385, y=150)
+username_input.place(x=450, y=150)
 
 # password input
-password = ctk.CTkLabel(root, text="Password:", font=('calibre',15))
-password_input = tk.Entry(root, textvariable=password_var, font=('calibre',12,'normal'), show="*", width=25)
-password.place(x=window_width - 50, y=145)
-password_input.place(x=window_width + 25, y=145)
+password = ctk.CTkLabel(root, text="Password:", font=('calibre',20))
+password_input = ctk.CTkEntry(root, textvariable=password_var, font=('calibre',20,'normal'), show="*", width=200)
+password.place(x=350, y=200)
+password_input.place(x=450, y=200)
 
 # luodaan teksti kenttä jossa teksti voi muuttua
 my_string_var.set("")
-text = ctk.CTkLabel(root, textvariable=my_string_var, font=('calibre',15))
-text.place(x=window_width + 70, y=170)
+text = ctk.CTkLabel(root, textvariable=my_string_var, font=('calibre',20))
+text.place(x=465, y=235)
 
 # luodaan tyylit buttoniin ja luodaan buttoni
 button = ctk.CTkButton(master=root, text="Login", command=get_input)
-button.place(x=window_width + 115, y=200)
+button.place(x=510, y=270)
 
 # jos painaa x:sää sulkee ikkunan
 def close():
